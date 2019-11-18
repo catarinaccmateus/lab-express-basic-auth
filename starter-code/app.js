fcb326451eed28995d0969c44cbba7e192e5a4d4
catarinaccmateus <catarina_1102@hotmail.com>
2019-11-18 17:48:54 +0000

    done, needs style

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,6 +1,8 @@
 'use strict';
 
-const { join } = require('path');
+const {
+  join
+} = require('path');
 const express = require('express');
 const createError = require('http-errors');
 const cookieParser = require('cookie-parser');
@@ -8,7 +10,16 @@ const logger = require('morgan');
 const sassMiddleware = require('node-sass-middleware');
 const serveFavicon = require('serve-favicon');
 
+const expressSession = require('express-session');
+const connectMongo = require('connect-mongo');
+const MongoStore = connectMongo(expressSession);
+const mongoose = require('mongoose');
+
+
 const indexRouter = require('./routes/index');
+const pagesRouter = require('./routes/pages'); //new router created for other logics
+
+const User = require('./models/user'); //I will use the User model since I want to use its parameters in the views
 
 const app = express();
 
@@ -18,7 +29,9 @@ app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(serveFavicon(join(__dirname, 'public/images', 'favicon.ico')));
 app.use(express.static(join(__dirname, 'public')));
@@ -29,7 +42,41 @@ app.use(sassMiddleware({
   sourceMap: true
 }));
 
+app.use(
+  expressSession({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 60 * 60 * 24 * 15,
+      sameSite: true,
+      httpOnly: true,
+      secure: process.env.NODE_ENV !== 'development'
+    },
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 60 * 60 * 24
+    })
+  })
+);
+
+app.use((req, res, next) => {
+  const userId = req.session.user;
+  if (userId) {
+    User.findById(userId)
+      .then(user => {
+        req.user = user;  //this will allow me to access the user through req at any point after being authenticated
+        res.locals.user = user; //So I can access the user in the views
+        next();
+      });
+  } else {
+    next();
+  }
+});
+
 app.use('/', indexRouter);
+app.use('/', pagesRouter); //added the new router to app use
+
 
 // Catch missing routes and forward to error handler
 app.use((req, res, next) => {
---
+++ starter-code/middleware/route-guard.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = (req,res,next) => {
+    if(req.user) {
+        next();
+    } else {
+        next(new Error('If you are not logged in, you cant see this page!'));
+    }
+};
+
